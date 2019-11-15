9c7848a496b31971e3d7262b2d230304f0a7adb2
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-15 14:48:32 +0100

    Add login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            msg: '',
+        };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.handleChange = this.handleChange.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+ 
+    login(event) {
+        const { state: { username, password }} = this;
+        const payload = { username, password };
+
+        axios.post('http://localhost:8000/rest-auth/login/', payload).then((response) => {
+            console.log(response.data)
+            this.setState({ msg: 'Login successfull' });
+        }).catch(() => {
+            this.setState({ msg: 'Login failed' });
+        });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <label>Login: </label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.login}
+                        onChange={this.handleChange}/>
+
+                    <label>Password: </label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}/>
+
+                    <input type="submit" value="Sign in"/>
+                </form>
+
+                <p>{this.state.msg}</p>
+            </div>
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Login />,
+  document.getElementById('root')
+);
